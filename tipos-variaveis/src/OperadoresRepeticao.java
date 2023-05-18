d96698cc1f48dcb3dfb78923b98b41c79bb3f626
Samuel Iansen <iansen2022@gmail.com>
2023-05-17 22:23:18 -0300

    Operadores de Repetição

---
+++ tipos-variaveis/src/OperadoresRepeticao.java
@@ -0,0 +1,12 @@
+public class OperadoresRepeticao {
+    public static void main(String[] args) {
+        int numero = 5;
+
+        //repetição
+        //numero ++;
+        //numro = numero + 1;
+
+        System.out.println(++ numero);
+        
+    }
+}

716b5a0df94f30db37dc798d3a525b6681e4055a
Samuel Iansen <iansen2022@gmail.com>
2023-05-17 22:48:51 -0300

    Operadore Boolean

---
+++ tipos-variaveis/src/OperadoresBoolean.java
@@ -0,0 +1,13 @@
+public class OperadoresBoolean {
+
+    public static void main(String[] args) {
+        
+        boolean variavel = true;
+        
+        //System.out.println(!variavel); inverte o valor da variável Boolean
+        variavel = !variavel;
+        
+        System.out.println(variavel);
+    }
+    
+}

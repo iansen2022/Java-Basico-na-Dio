c16e5266ca461717addf5f0e46accec52a5ac74c
Samuel Iansen <iansen2022@gmail.com>
2023-05-18 20:05:27 -0300

    OperadoresTernario

---
+++ tipos-variaveis/src/OperadoresTernario.java
@@ -0,0 +1,24 @@
+public class OperadoresTernario {
+    public static void main(String[] args) {
+        
+        int a, b;
+        a = 5;
+        b = 6;
+
+        String resultado = a == b ?"verdadeiro" : "falso"; 
+        int resultado2 = a == b ? 1 : 0; 
+
+
+        //String resultado = "";
+        // if (a == b) {
+        //     resultado = "verdadeiro";
+        //} else 
+        //     resultado = "falso";{
+            
+        // }
+        System.out.println(resultado);
+        System.out.println(resultado2);
+
+
+    }
+}

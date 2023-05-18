1514fb9e5b46e2071f4385199c7d3b3c3bd98557
Samuel Iansen <iansen2022@gmail.com>
2023-05-17 21:21:30 -0300

    Tipos de variaveis e concatenação

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,24 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+        //estudar variáveis tipos primitivos
+        //estudar a class string que representa texto na aplicação
+        //String meuNome "Samuel Iansen";
+
         System.out.println("Hello, World!");
+
+        double salarioMinimo = 2500;
+        System.out.println(salarioMinimo);
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+        System.out.println(VALOR_DE_PI);
+
     }
 }
